794331a01ebd50f92d0b34617ea230b51f3cb3db
francesco <francesco.peluso@leonardocompany.com>
2023-07-07 09:31:14 +0200

    V1.1 (by user1)

--- Submodules/modB/Submodules/submodB_1/Functions/modB_func1.m
+++ Submodules/modB/Submodules/submodB_1/Functions/modB_func1.m
@@ -1,7 +1,7 @@
 function [outputArg1,outputArg2] = modB_func1(inputArg1,inputArg2)
 %MODA_FUNC1 Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = -inputArg1;
+outputArg1 = -inputArg1+50;
 outputArg2 = -abs(inputArg2);
 end
 
