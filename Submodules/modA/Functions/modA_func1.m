7085b30b2ab4afecc1033795bbbf8db3fe131f1c
francesco <francesco.peluso@leonardocompany.com>
2023-07-07 09:23:29 +0200

    V1.1 (by user3)

--- Submodules/modA/Functions/modA_func1.m
+++ Submodules/modA/Functions/modA_func1.m
@@ -1,7 +1,7 @@
 function [outputArg1,outputArg2] = modA_func1(inputArg1,inputArg2)
 %MODA_FUNC1 Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1.^2;
+outputArg1 = inputArg1.^2+10;
 outputArg2 = -inputArg2.^2;
 end
 
